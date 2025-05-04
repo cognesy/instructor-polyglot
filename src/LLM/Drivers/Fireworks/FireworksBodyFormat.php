2b9c3dee3d3734071551cef970319b27c2946c1f
ddebowczyk <ddebowczyk@gmail.com>
2025-05-04 10:51:24 +0000

    Release version 0.17.0

--- src/LLM/Drivers/Fireworks/FireworksBodyFormat.php
+++ src/LLM/Drivers/Fireworks/FireworksBodyFormat.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Cognesy\Polyglot\LLM\Drivers\GeminiOAI;
+namespace Cognesy\Polyglot\LLM\Drivers\Fireworks;
 
 use Cognesy\Polyglot\LLM\Drivers\OpenAICompatible\OpenAICompatibleBodyFormat;
 use Cognesy\Polyglot\LLM\Enums\OutputMode;
 
-class GeminiOAIBodyFormat extends OpenAICompatibleBodyFormat
+class FireworksBodyFormat extends OpenAICompatibleBodyFormat
 {
     protected function applyMode(
         array        $request,
@@ -16,7 +16,6 @@ class GeminiOAIBodyFormat extends OpenAICompatibleBodyFormat
     ) : array {
         switch($mode) {
             case OutputMode::Json:
-            case OutputMode::JsonSchema:
                 $request['response_format'] = [
                     'type' => 'json_object'
                 ];
@@ -25,8 +24,14 @@ class GeminiOAIBodyFormat extends OpenAICompatibleBodyFormat
             case OutputMode::MdJson:
                 $request['response_format'] = ['type' => 'text'];
                 break;
+            case OutputMode::JsonSchema:
+                $request['response_format'] = [
+                    'type' => 'json_object',
+                    'schema' => $responseFormat['json_schema']['schema'] ?? $responseFormat['schema'] ?? [],
+                ];
+                break;
             case OutputMode::Unrestricted:
-                $request['response_format'] = $request['response_format'] ?? ['type' => 'json_object'] ?? [];
+                $request['response_format'] = $request['response_format'] ?? $responseFormat ?? [];
                 break;
         }
 
@@ -36,6 +41,3 @@ class GeminiOAIBodyFormat extends OpenAICompatibleBodyFormat
         return array_filter($request);
     }
 }
-
-// Add support for:
-// "reasoning_effort": "low", "medium", "high", "none"
